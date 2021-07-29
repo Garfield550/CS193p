ecbe1bf14b7663bfe94763135e0d18a10fac5a0c
Garfield Lee <openpgp@550.moe>
2021-07-29 12:22:39 +0800

    feat: start lecture 1

---
+++ Memorize/Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Garfield Lee on 7/29/21.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
